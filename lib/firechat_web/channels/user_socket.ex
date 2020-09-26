08ebcc0d873332120ddd3e9ef5b8da5d20a5b4d6
Randy <jorabart@gmail.com>
2020-09-26 22:27:14 +0000

    Chat room up and running.

--- lib/firechat_web/channels/user_socket.ex
+++ lib/firechat_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule FirechatWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", FirechatWeb.RoomChannel
+    channel "room:*", FirechatWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
