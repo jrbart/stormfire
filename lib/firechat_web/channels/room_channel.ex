e648fb2680ce20cbc7345c9b1fc6d2214cbc3e2e
Randy <jorabart@gmail.com>
2021-02-07 17:18:31 +0000

    After a long hiatus

--- lib/firechat_web/channels/room_channel.ex
+++ lib/firechat_web/channels/room_channel.ex
@@ -1,14 +1,32 @@
 defmodule FirechatWeb.RoomChannel do
   use Phoenix.Channel
 
-  def join("room:lobby", _message, socket) do
-    {:ok, socket}
+  def join("room:lobby", payload, socket) do
+    if authorized?(payload) do
+      {:ok, socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
   end
-  def join("room:" <> private_room_id, _params, _socket) do
+
+  def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
   end
+
+  # Replies only to client (for testing that socket is open)
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  # Broadcasts to all connections
   def handle_in("new_msg", %{"body" => body}, socket) do
     broadcast!(socket, "new_msg", %{body: body})
     {:noreply, socket}
+  end
+
+  # Add authorization logic here as required.
+  # For starters everyonw is allowed
+  defp authorized?(_payload) do
+    true
   end
 end
